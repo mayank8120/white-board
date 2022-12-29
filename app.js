8100030d3bf433143436d9d432e348fadec401d8
mayank8120 <mayanklaxkar11@gmail.com>
2022-12-29 18:46:20 +0530

    Open oard to launch

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+
+const socket = require('socket.io');
+
+
+const app = express();
+
+
+
+app.use(express.static("public"));
+
+
+let port = 5000;
+let server = app.listen(port, () => {
+    console.log('listening to port 5000');
+});
+
+
+let io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log("Socket Connection Made");
+
+
+    socket.on("beginPath", (data) => {
+        io.sockets.emit("beginPath", data);
+    })
+
+
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+    })
+
+
+    socket.on("redoUndo", (data) => {
+        io.sockets.emit("redoUndo", data);
+    })
+
+
+});
